fd4a0e5e5e88fc0fd53a03365d4f3d5a5a8bcaae
BarbaraHdzS <barbara.hdz.sanch@gmail.com>
2025-04-28 07:52:43 -0600

    add a class for buttons

--- react-complete-guide-course-resources/attachments/03 React Essentials/01-starting-project/src/components/TabButton.jsx
+++ react-complete-guide-course-resources/attachments/03 React Essentials/01-starting-project/src/components/TabButton.jsx
@@ -7,7 +7,7 @@ export default function TabButton ({children, onSelect}){
 
     return (
         <li>
-            <button onClick ={onSelect}>{children}</button>
+            <button className= "active" onClick ={onSelect}>{children}</button>
         </li>
     );
 }
